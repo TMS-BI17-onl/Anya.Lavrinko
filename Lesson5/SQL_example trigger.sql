5c12dd1bcd2f2cf17dacceaf373a8cdee087d05d
alavrinko <alavrinko@gmail.com>
2023-02-05 15:01:50 +0400

    HW added

---
+++ Lesson5/SQL_example trigger.sql
@@ -0,0 +1,71 @@
+﻿CREATE DATABASE productsdb;
+GO
+USE productsdb;
+CREATE TABLE Products
+(
+    Id INT IDENTITY PRIMARY KEY,
+    ProductName NVARCHAR(30) NOT NULL,
+    Manufacturer NVARCHAR(20) NOT NULL,
+    ProductCount INT DEFAULT 0,
+    Price MONEY NOT NULL
+);
+CREATE TABLE History 
+(
+    Id INT IDENTITY PRIMARY KEY,
+    ProductId INT NOT NULL,
+    Operation NVARCHAR(200) NOT NULL,
+    CreateAt DATETIME NOT NULL DEFAULT GETDATE(),
+);
+
+USE productsdb
+GO
+CREATE TRIGGER Products_INSERT
+ON Products
+AFTER INSERT
+AS
+INSERT INTO History (ProductId, Operation)
+SELECT Id, 'Добавлен товар ' + ProductName + '   фирма ' + Manufacturer
+FROM INSERTED
+
+USE productsdb;
+INSERT INTO Products (ProductName, Manufacturer, ProductCount, Price)
+VALUES('iPhone X', 'Apple', 2, 79900)
+ 
+SELECT * FROM History
+
+USE productsdb
+GO
+CREATE TRIGGER Products_DELETE
+ON Products
+AFTER DELETE
+AS
+INSERT INTO History (ProductId, Operation)
+SELECT Id, 'Удален товар ' + ProductName + '   фирма ' + Manufacturer
+FROM DELETED
+
+SELECT *
+FROM Products
+
+USE productsdb;
+DELETE FROM Products
+WHERE Id=2
+ 
+SELECT * FROM History
+
+USE productsdb
+GO
+CREATE TRIGGER Products_UPDATE
+ON Products
+AFTER UPDATE
+AS
+INSERT INTO History (ProductId, Operation)
+SELECT Id, 'Обновлен товар ' + ProductName + '   фирма ' + Manufacturer
+FROM INSERTED
+
+USE productsdb;
+INSERT INTO Products(ProductName,Manufacturer,ProductCount,Price)
+VALUES ('C350','Motorola', 10,2100)
+
+ UPDATE Products SET Manufacturer='Moto'
+ WHERE Manufacturer='Motorola'
+SELECT* FROM History
